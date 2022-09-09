cc4086846853a26aa4e23b1544fa8c608fb722b7
dani mulyana <daniwgs92@gmail.com>
2022-09-09 10:49:03 +0700

    View Engine with EJS

--- server.js
+++ server.js
@@ -3,21 +3,39 @@ const app = express()
 const port = 3000
 
 
+
+// View engine 'ejs'
+app.set('view engine','ejs')
+
 // mendefinisikan route path (/) yang merender index.html
 app.get('/',(req,res) => {
-	res.sendFile(__dirname +'/views/index.html');
+	res.render('index', {
+		name : "Dani",
+		title : "Web Express"
+	}),
 	res.status(200);
 });
 
 // mendefinisikan route path (/about) yang merender about.html
 app.get('/about',(req,res) => {
-	res.sendFile(__dirname +'/views/about.html');
+	res.render('about');
 });
 
 // mendefinisikan route path (/contact) yang merender contact.html
 app.get('/contact',(req,res) => {
-	res.sendFile(__dirname +'/views/contact.html');
+	res.render('contact');
 });
+
+
+
+app.get('/product/:id',(req,res) => {
+	res.send('product id : '+ req.params.id <br> + req.query.kategori);
+});
+
+
+// app.get('/product/:id/?kategori',(req,res) => {
+// 	res.send('product id : '+ req.params.id +" "+ req.params.kategori);
+// });
 
 // mendefinisikan middleware test
 // app.use('/',(req, res)=>{
